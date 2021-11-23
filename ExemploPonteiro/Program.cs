2fb56c6abebc33975256a9ac607dc1025c842b80
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2021-11-23 18:42:55 -0300

    Conteudo Ponteiros

--- ExemploPonteiro/Program.cs
+++ ExemploPonteiro/Program.cs
@@ -6,6 +6,33 @@ namespace ExemploPonteiro
     {
         static void Main(string[] args)
         {
+            /*
+             * “Ponteiro é uma variável que contém um endereço da memória. Esse endereço é normalmente a posição de uma outra variável na memória.”
+
+
+Fornece meios para
+Funções modificarem seus argumentos (passagem por referência)
+Implementar alocação dinâmica de memória
+Aumentar a eficiência de certas rotinas
+
+
+Declaração
+tipo* nome_da_variável;
+tipo é qualquer tipo de dado em C# e indica o tipo base do ponteiro, ou seja, para que tipo de dado o ponteiro deve apontar. O conteúdo armazenado na variável ponteiro é sempre um endereço, independente do tipo base.
+nome_da_variável é o nome da variável ponteiro para o tipo especificado.
+
+
+Operador &
+É um operador unário.
+Devolve o endereço da memória do operando.
+int *p;
+int a=5, q;
+...
+p = &a; //ponteiro recebe o endereço da variável a
+
+Neste exemplo, se a variável ‘a’ ocupasse a posição de memória 1250, o valor da variável ‘p’ seria 1250.
+
+            */
             unsafe
             {
                 int i;
