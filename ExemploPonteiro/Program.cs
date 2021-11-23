de21423b7b13aec3215844a1e0a75602d3bdbc8d
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2021-11-23 17:59:43 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploPonteiro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExemploPonteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int i;
+                i = 10;
+                int* ponteiro;
+                ponteiro = &i;
+                Console.WriteLine("O valor que o ponteiro está apontando é = "+ *ponteiro);
+            }
+        }
+    }
+}
